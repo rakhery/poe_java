ea13b5289223165723cf90be3f1eae5d9dfa5a02
Christian RAKOTO <crakoto@yahoo.fr>
2022-07-05 10:59:24 +0200

    Hello World M2i Tours

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world M2i TOURS");
+        System.out.println("Hello world M2i TOURS St Avertin");
     }
 }

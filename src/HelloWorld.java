d904b10c5c904698b70021d71ba63a27877af2bf
Christian RAKOTO <crakoto@yahoo.fr>
2022-07-05 11:57:56 +0200

    ajout calcul de perimètre et la surface

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,19 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world M2i TOURS St Avertin");
+        int longueur=200;
+//        longueur=20;
+//        longueur=35;
+
+        int largeur=50;
+//        largeur=20;
+//        largeur=10;
+
+        int perimetre=2*(longueur+largeur);
+        int surface=longueur*largeur;
+        System.out.println(perimetre);
+        System.out.println(surface);
+
+
     }
 }

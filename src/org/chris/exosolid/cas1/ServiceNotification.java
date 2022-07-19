56c185e1e2295530bea97c5cbdaa40fd6f947d60
Christian RAKOTO <crakoto@yahoo.fr>
2022-07-19 12:11:52 +0200

    4-Implementer le service notification à partir du contrat INotificationService

--- src/org/chris/exosolid/cas1/ServiceNotification.java
+++ src/org/chris/exosolid/cas1/ServiceNotification.java
@@ -8,25 +8,13 @@ public class ServiceNotification {
     //La classe Bank service prend plusieurs responsabilités
     //Il faut créer des services différents
     public void sendOTP(String medium) {
-        if (medium.equals("email")) {
-            // write email related logic
-        } else if (medium.equals("mobile")) {
-            //  write SMS related logic
-        } else if (medium.equals("whatsapp")) {
-            //  write Whatsapp related logic
-        }
+
     }
 
     //    Envoyer une notification en guise de rapport pour chaque
 //    Par email ou mobile ou whatsapp
     public void sendTransactionReport(String medium) {
-        if (medium.equals("email")) {
-            // write email related logic
-        } else if (medium.equals("mobile")) {
-            //  write SMS related logic
-        } else if (medium.equals("whatsapp")) {
-            //  write Whatsapp related logic
-        }
+
     }
 
 }

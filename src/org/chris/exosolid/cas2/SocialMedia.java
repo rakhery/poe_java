daf3ee4793dcb05de4a89f844f0ed1a0fe70b76b
Christian RAKOTO <crakoto@yahoo.fr>
2022-07-19 12:32:59 +0200

    1- Le Problème en perspective de la classe Instangram n'a pas de methode groupVideoCall dans la classe SocialMedia

--- src/org/chris/exosolid/cas2/SocialMedia.java
+++ src/org/chris/exosolid/cas2/SocialMedia.java
@@ -10,7 +10,7 @@ abstract class SocialMedia {
 
     //@support WhatsApp,Facebook,Instagram
     public abstract void sendPhotosAndVideos();
-
+    //Problème de la classe Instangram n'a pas de methode groupVideoCall
     //@support WhatsApp,Facebook
     public abstract void groupVideoCall(String... users);
 }

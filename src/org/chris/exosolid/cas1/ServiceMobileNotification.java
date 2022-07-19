d7e64bcaf7d8f048439f8d094d13fa02962ea579
Christian RAKOTO <crakoto@yahoo.fr>
2022-07-19 12:26:56 +0200

    5-Implementer le service notification à partir du contrat INotificationService des trois service

---
+++ src/org/chris/exosolid/cas1/ServiceMobileNotification.java
@@ -0,0 +1,13 @@
+package org.chris.exosolid.cas1;
+
+public class ServiceMobileNotification implements INotificationService{
+    @Override
+    public void sendOTP(String medium) {
+        //  write SMS related logic
+    }
+
+    @Override
+    public void sendTransactionReport(String medium) {
+        //  write SMS related logic
+    }
+}
---
+++ src/org/chris/exosolid/cas1/ServiceWhatsAppNotification.java
@@ -0,0 +1,13 @@
+package org.chris.exosolid.cas1;
+
+public class ServiceWhatsAppNotification implements INotificationService{
+    @Override
+    public void sendOTP(String medium) {
+        //  write Whatsapp related logic
+    }
+
+    @Override
+    public void sendTransactionReport(String medium) {
+        //  write Whatsapp related logic
+    }
+}

e6160377b398ffaea2d01a10be5c9b2d44174e02
Christian RAKOTO <crakoto@yahoo.fr>
2022-07-19 12:44:07 +0200

    3-Proposition de Solution: Séparer en imposant un contrat dans des interfaces    * expemple: Interface IGestionPost, IGestionMedia, IGestionChat

--- src/org/chris/exosolid/cas2/SocialMedia.java
+++ src/org/chris/exosolid/cas2/SocialMedia.java
@@ -1,5 +1,13 @@
 package org.chris.exosolid.cas2;
 
+/**
+ *
+ * Proposition de Solution: Séparer en imposant un contrat dans des interfaces
+ * expemple: Interface IGestionPost, IGestionMedia, IGestionChat
+ * permet de countourner l'héritage multiple et respecter le principe de Liskov
+ */
+
+
 abstract class SocialMedia {
 
     //@support WhatsApp,Facebook,Instagram
